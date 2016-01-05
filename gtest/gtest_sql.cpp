6c067386f4d4dde37aebe9c51dcf0c8ccbedea71
kasia test <kasia@test.pl>
2016-01-05 15:40:38 +0100

    test cases

--- gtest/gtest_sql.cpp
+++ gtest/gtest_sql.cpp
@@ -9,63 +9,346 @@ using namespace std;
 class SQLTest: public ::testing::Test {
 protected:
 	SQL *sql;
-  virtual void SetUp() {
-  }
-  virtual void TearDown() { }
-  SQLTest(){
-	  sql= new SQL();
-  }
-  ~SQLTest(){
-	  delete sql;
-  }
+	virtual void SetUp() {
+	}
+	virtual void TearDown() {
+	}
+	SQLTest() {
+		sql = new SQL();
+	}
+	~SQLTest() {
+		delete sql;
+	}
 };
-
-struct sql_state : SQL{
-	string sqlString;
-	bool flag_count:1;//=false;
-	bool flag_all:1;//=false;
-	std::vector<std::pair<std::string, std::string>> expression;
-	std::vector<std::pair<std::string, std::string>> where_condition;
-	bool success;
-};
-
 
 TEST_F(SQLTest, basicSelect) {
-	EXPECT_EQ (false, sql->readSQL( std::string("select * from Customer"),std::string("Customer1")));
-	EXPECT_EQ (true, sql->readSQL( std::string("select * from Customer"),std::string("Customer")));
-	EXPECT_EQ (true, sql->readSQL( std::string("select * from Customer where test =test"),std::string("Customer")));
-	EXPECT_EQ (false, sql->readSQL( std::string("select * from Customer where test, test"),std::string("Customer")));
-	EXPECT_EQ (false, sql->readSQL( std::string("select * fromCustomer"),std::string("Customer")));
-	EXPECT_EQ (true, sql->readSQL( std::string("select count(*) from Customer"),std::string("Customer")));
-	EXPECT_EQ (true, sql->readSQL( std::string("select count(*) from Customer where imie =test"),std::string("Customer")));
-	EXPECT_EQ (true, sql->readSQL( std::string("select imie, test from Customer"),std::string("Customer")));
-	EXPECT_EQ (true, sql->readSQL( std::string("select imire,test from Customer where imie =test"),std::string("Customer")));
+	EXPECT_EQ(false,
+			sql->readSQL(std::string("select * from Customer"),
+					std::string("Customer1")));
+	EXPECT_EQ(true,
+			sql->readSQL(std::string("select * from Customer"),
+					std::string("Customer")));
+	EXPECT_EQ(true,
+			sql->readSQL(std::string("select * from Customer where test =test"),
+					std::string("Customer")));
+	EXPECT_EQ(false,
+			sql->readSQL(std::string("select * from Customer where test, test"),
+					std::string("Customer")));
+	EXPECT_EQ(false,
+			sql->readSQL(std::string("select * fromCustomer"),
+					std::string("Customer")));
+	EXPECT_EQ(true,
+			sql->readSQL(std::string("select count(*) from Customer"),
+					std::string("Customer")));
+	EXPECT_EQ(true,
+			sql->readSQL(
+					std::string(
+							"select count(*) from Customer where imie =test"),
+					std::string("Customer")));
+	EXPECT_EQ(true,
+			sql->readSQL(std::string("select imie, test from Customer"),
+					std::string("Customer")));
+	EXPECT_EQ(true,
+			sql->readSQL(
+					std::string(
+							"select imire,test from Customer where imie =test"),
+					std::string("Customer")));
+}
+
+TEST_F(SQLTest, selectchekingFlag) {
+	SQL sqla, sqlb, sqlc, sqld;
+	std::pair<std::string, std::string> pairs;
+	string first, second;
+	sqla.readSQL(std::string("select * from Customer"),
+			std::string("Customer"));
+	EXPECT_EQ('S', sqla.statement);
+	EXPECT_EQ(true, sqla.flag_all);
+	EXPECT_EQ(false, sqla.flag_count);
+	EXPECT_EQ(0, sqla.expression.size());
+	EXPECT_EQ(0, sqla.where_condition.size());
+
+	sqlb.readSQL(std::string("select count(*) from Customer where imie = test"),
+			std::string("Customer"));
+	EXPECT_EQ('S', sqla.statement);
+	EXPECT_EQ(false, sqlb.flag_all);
+	EXPECT_EQ(true, sqlb.flag_count);
+	EXPECT_EQ(0, sqlb.expression.size());
+	EXPECT_EQ(1, sqlb.where_condition.size());
+
+	sqlc.readSQL(std::string("select imie from Customer where imie = 'test'"),
+			std::string("Customer"));
+	EXPECT_EQ('S', sqla.statement);
+	EXPECT_EQ(false, sqlc.flag_all);
+	EXPECT_EQ(false, sqlc.flag_count);
+	EXPECT_EQ(1, sqlc.expression.size());
+	EXPECT_EQ(1, sqlc.where_condition.size());
+	pairs = sqlc.expression.back();
+	first = pairs.first;
+	second = pairs.second;
+	EXPECT_STREQ("IMIE", first.c_str());
+	EXPECT_STREQ("", second.c_str());
+	pairs = sqlc.where_condition.back();
+	first = pairs.first;
+	second = pairs.second;
+	EXPECT_STREQ("IMIE", first.c_str());
+	EXPECT_STREQ("TEST", second.c_str());
+
+
+	sqld.readSQL(std::string("select imie,test from Customer where imie=test"),
+			std::string("Customer"));
+	EXPECT_EQ('S', sqla.statement);
+	EXPECT_EQ(false, sqld.flag_all);
+	EXPECT_EQ(false, sqld.flag_count);
+	EXPECT_EQ(2, sqld.expression.size());
+	EXPECT_EQ(1, sqld.where_condition.size());
+	pairs = sqld.expression.back();
+	first = pairs.first;
+	second = pairs.second;
+	EXPECT_STREQ("TEST", first.c_str());
+	EXPECT_STREQ("", second.c_str());
+	sqld.expression.pop_back();
+	pairs = sqld.expression.back();
+	first = pairs.first;
+	second = pairs.second;
+	EXPECT_STREQ("IMIE", first.c_str());
+	EXPECT_STREQ("", second.c_str());
+	pairs = sqld.where_condition.back();
+	first = pairs.first;
+	second = pairs.second;
+	EXPECT_STREQ("IMIE", first.c_str());
+	EXPECT_STREQ("TEST", second.c_str());
+
 }
 
 TEST_F(SQLTest, basicInsert) {
-	EXPECT_EQ (false, sql->readSQL( std::string("insert into customer"),std::string("Customer1")));
-	EXPECT_EQ (false, sql->readSQL( std::string("insert into Customer"),std::string("Customer")));
-	EXPECT_EQ (true, sql->readSQL( std::string("insert into Customer values (wwww,www,12)"),std::string("Customer")));
-	EXPECT_EQ (false, sql->readSQL( std::string("insert into Customer values wwww,www,12"),std::string("Customer")));
-	EXPECT_EQ (false, sql->readSQL( std::string("insert into Customer values wwww;www;12"),std::string("Customer")));
-	EXPECT_EQ (false, sql->readSQL( std::string("insert into Customer values ,,"),std::string("Customer")));
+	EXPECT_EQ(false,
+			sql->readSQL(std::string("insert into customer"),
+					std::string("Customer1")));
+	EXPECT_EQ(false,
+			sql->readSQL(std::string("insert into Customer"),
+					std::string("Customer")));
+	EXPECT_EQ(true,
+			sql->readSQL(
+					std::string("insert into Customer values (wwww,www,12)"),
+					std::string("Customer")));
+	EXPECT_EQ(false,
+			sql->readSQL(std::string("insert into Customer values wwww,www,12"),
+					std::string("Customer")));
+	EXPECT_EQ(false,
+			sql->readSQL(std::string("insert into Customer values wwww;www;12"),
+					std::string("Customer")));
+	EXPECT_EQ(false,
+			sql->readSQL(std::string("insert into Customer values ,,"),
+					std::string("Customer")));
+}
+
+TEST_F(SQLTest, InsertchekingFlag) {
+	SQL sqla, sqlb, sqlc;
+	std::pair<std::string, std::string> pairs;
+	string first, second;
+	sqla.readSQL(std::string("Insert into Customer"), std::string("Customer"));
+	EXPECT_EQ('0', sqla.statement);
+	EXPECT_EQ(false, sqla.flag_all);
+	EXPECT_EQ(false, sqla.flag_count);
+	EXPECT_EQ(0, sqla.expression.size());
+	EXPECT_EQ(0, sqla.where_condition.size());
+
+	sqlb.readSQL(
+			std::string("insert into Customer values (imie, nazwisko, wiek)"),
+			std::string("Customer"));
+	EXPECT_EQ('I', sqlb.statement);
+	EXPECT_EQ(3, sqlb.expression.size());
+	EXPECT_EQ(0, sqlb.where_condition.size());
+	pairs = sqlb.expression.back();
+	first = pairs.first;
+	second = pairs.second;
+	EXPECT_STREQ("WIEK", first.c_str());
+	EXPECT_STREQ("", second.c_str());
+	sqlb.expression.pop_back();
+	pairs = sqlb.expression.back();
+	first = pairs.first;
+	second = pairs.second;
+	EXPECT_STREQ("NAZWISKO", first.c_str());
+	EXPECT_STREQ("", second.c_str());
+	sqlb.expression.pop_back();
+	pairs = sqlb.expression.back();
+	first = pairs.first;
+	second = pairs.second;
+	EXPECT_STREQ("IMIE", first.c_str());
+	EXPECT_STREQ("", second.c_str());
+
+
+	sqlc.readSQL(std::string("insert into from Customer where imie = test"),
+			std::string("Customer"));
+	EXPECT_EQ('0', sqlc.statement);
+	EXPECT_EQ(0, sqlc.expression.size());
+	EXPECT_EQ(0, sqlc.where_condition.size());
+
 }
 
 TEST_F(SQLTest, basicUpdate) {
-	EXPECT_EQ (false, sql->readSQL( std::string("Update Customer set "),std::string("Customer1")));
-	EXPECT_EQ (false, sql->readSQL( std::string("Update Customer set "),std::string("Customer")));
-	EXPECT_EQ (true, sql->readSQL( std::string("Update Customer set test= test where cos =cps"),std::string("Customer")));
-	EXPECT_EQ (false, sql->readSQL( std::string("Update Customer set "),std::string("Customer1")));
-	EXPECT_EQ (true, sql->readSQL( std::string("Update Customer set test= test where cos =cps"),std::string("Customer")));
-	EXPECT_EQ (true, sql->readSQL( std::string("Update Customer set test = test where cos =cps"),std::string("Customer")));
-	EXPECT_EQ (false, sql->readSQL( std::string("Update Customer set test,test1 where cos =cps"),std::string("Customer")));
+	EXPECT_EQ(false,
+			sql->readSQL(std::string("Update Customer set "),
+					std::string("Customer1")));
+	EXPECT_EQ(false,
+			sql->readSQL(std::string("Update Customer set "),
+					std::string("Customer")));
+	EXPECT_EQ(true,
+			sql->readSQL(
+					std::string(
+							"Update Customer set test= test where cos =cps"),
+					std::string("Customer")));
+	EXPECT_EQ(false,
+			sql->readSQL(std::string("Update Customer set "),
+					std::string("Customer1")));
+	EXPECT_EQ(true,
+			sql->readSQL(
+					std::string(
+							"Update Customer set test= test where cos =cps"),
+					std::string("Customer")));
+	EXPECT_EQ(true,
+			sql->readSQL(
+					std::string(
+							"Update Customer set test = test where cos =cps"),
+					std::string("Customer")));
+	EXPECT_EQ(false,
+			sql->readSQL(
+					std::string(
+							"Update Customer set test,test1 where cos =cps"),
+					std::string("Customer")));
 }
 
+TEST_F(SQLTest, UpdatechekingFlag) {
+	SQL sqla, sqlb, sqlc, sqld, sqle, sqlf;
+	std::pair<std::string, std::string> pairs;
+	string first, second;
+	sqla.readSQL(std::string("Update Customer set "), std::string("Customer"));
+	EXPECT_EQ('0', sqla.statement);
+	EXPECT_EQ(false, sqla.flag_all);
+	EXPECT_EQ(false, sqla.flag_count);
+	EXPECT_EQ(0, sqla.expression.size());
+	EXPECT_EQ(0, sqla.where_condition.size());
+
+	sqlb.readSQL(std::string("Update Customer set test=test1 where imie=ania"),
+			std::string("Customer"));
+	EXPECT_EQ('U', sqlb.statement);
+	EXPECT_EQ(1, sqlb.expression.size());
+	EXPECT_EQ(1, sqlb.where_condition.size());
+	pairs = sqlb.expression.back();
+	first = pairs.first;
+	second = pairs.second;
+	EXPECT_STREQ("TEST", first.c_str());
+	EXPECT_STREQ("TEST1", second.c_str());
+
+	pairs = sqlb.where_condition.back();
+	first = pairs.first;
+	second = pairs.second;
+	EXPECT_STREQ("IMIE", first.c_str());
+	EXPECT_STREQ("ANIA", second.c_str());
+
+
+	sqle.readSQL(
+			std::string("Update Customer set test = est1 where imie= kon"),
+			std::string("Customer"));
+	EXPECT_EQ('U', sqle.statement);
+	EXPECT_EQ(1, sqle.expression.size());
+	EXPECT_EQ(1, sqle.where_condition.size());
+
+	pairs = sqle.expression.back();
+	first = pairs.first;
+	second = pairs.second;
+	EXPECT_STREQ("TEST", first.c_str());
+	EXPECT_STREQ("EST1", second.c_str());
+	pairs = sqle.where_condition.back();
+	first = pairs.first;
+	second = pairs.second;
+	EXPECT_STREQ("IMIE", first.c_str());
+	EXPECT_STREQ("KON", second.c_str());
+
+	sqlc.readSQL(std::string("Update Customer set test where cos = cps"),
+			std::string("Customer"));
+	EXPECT_EQ('0', sqlc.statement);
+	EXPECT_EQ(0, sqlc.expression.size());
+	EXPECT_EQ(0, sqlc.where_condition.size());
+
+	sqld.readSQL(std::string("Update Customer set test= where cos = cps"),
+			std::string("Customer"));
+	EXPECT_EQ('0', sqld.statement);
+	EXPECT_EQ(0, sqld.expression.size());
+	EXPECT_EQ(0, sqld.where_condition.size());
+
+	sqlf.readSQL(std::string("Update Customer set where cos = cps"),
+			std::string("Customer"));
+	EXPECT_EQ('0', sqlf.statement);
+	EXPECT_EQ(0, sqlf.expression.size());
+	EXPECT_EQ(0, sqlf.where_condition.size());
+}
 
 TEST_F(SQLTest, basicDelete) {
-	EXPECT_EQ (false, sql->readSQL( std::string("Delete from Customer "),std::string("Customer1")));
-	EXPECT_EQ (true, sql->readSQL( std::string("Delete from customer"),std::string("Customer")));
-	EXPECT_EQ (true, sql->readSQL( std::string("delete from customer where cos =cps"),std::string("Customer")));
-	EXPECT_EQ (false, sql->readSQL( std::string("delete from customer where cos ,cps"),std::string("Customer")));
-	EXPECT_EQ (false, sql->readSQL( std::string("delete from customer set test = test where cos =cps"),std::string("Customer")));
+	EXPECT_EQ(false,
+			sql->readSQL(std::string("Delete from Customer "),
+					std::string("Customer1")));
+	EXPECT_EQ(true,
+			sql->readSQL(std::string("Delete from customer"),
+					std::string("Customer")));
+	EXPECT_EQ(true,
+			sql->readSQL(std::string("delete from customer where cos =cps"),
+					std::string("Customer")));
+	EXPECT_EQ(false,
+			sql->readSQL(std::string("delete from customer where cos ,cps"),
+					std::string("Customer")));
+	EXPECT_EQ(false,
+			sql->readSQL(
+					std::string(
+							"delete from customer set test = test where cos =cps"),
+					std::string("Customer")));
+}
+
+TEST_F(SQLTest, deletetchekingFlag) {
+	SQL sqla, sqlb, sqlc, sqld;
+	std::pair<std::string, std::string> pairs;
+	string first, second;
+	sqla.readSQL(std::string("delete from Customer"),
+			std::string("Customer"));
+	EXPECT_EQ('D', sqla.statement);
+	EXPECT_EQ(false, sqla.flag_all);
+	EXPECT_EQ(false, sqla.flag_count);
+	EXPECT_EQ(0, sqla.expression.size());
+	EXPECT_EQ(0, sqla.where_condition.size());
+
+	sqlb.readSQL(std::string("delete (*) from Customer where imie = test"),
+			std::string("Customer"));
+	EXPECT_EQ('0', sqlb.statement);
+	EXPECT_EQ(false, sqlb.flag_all);
+	EXPECT_EQ(false, sqlb.flag_count);
+	EXPECT_EQ(0, sqlb.expression.size());
+	EXPECT_EQ(0, sqlb.where_condition.size());
+
+	int flag = sqlc.readSQL(std::string("delete from Customer where imie = 'test'"),
+			std::string("Customer"));
+	EXPECT_EQ(true, flag);
+	EXPECT_EQ('D', sqlc.statement);
+	EXPECT_EQ(false, sqlc.flag_all);
+	EXPECT_EQ(false, sqlc.flag_count);
+	EXPECT_EQ(0, sqlc.expression.size());
+	EXPECT_EQ(1, sqlc.where_condition.size());
+	pairs = sqlc.where_condition.back();
+	first = pairs.first;
+	second = pairs.second;
+	EXPECT_STREQ("IMIE", first.c_str());
+	EXPECT_STREQ("TEST", second.c_str());
+
+
+	sqld.readSQL(std::string("delete from Customer where imie=test"),
+			std::string("Customer"));
+	EXPECT_EQ('D', sqld.statement);
+	EXPECT_EQ(false, sqld.flag_all);
+	EXPECT_EQ(false, sqld.flag_count);
+	EXPECT_EQ(0, sqld.expression.size());
+	EXPECT_EQ(1, sqld.where_condition.size());
+	pairs = sqld.where_condition.back();
+	first = pairs.first;
+	second = pairs.second;
+	EXPECT_STREQ("IMIE", first.c_str());
+	EXPECT_STREQ("TEST", second.c_str());
 }
--- src/SQL.cpp
+++ src/SQL.cpp
@@ -7,21 +7,25 @@
 
 #include "SQL.h"
 
-void SQL::printAll() {
-		std::cout << this->statement << "flag_count " << this->flag_count
-				<< "flag_all " << this->flag_all << "\n";
-		for (auto a = this->expression.begin(); a != this->expression.end();
-				++a) {
-			std::cout << (*a).first << " " << (*a).second;
-		}
-		std::cout << "\n";
-		for (auto a = this->where_condition.begin();
-				a != this->where_condition.end(); ++a) {
-			std::cout << (*a).first << " " << (*a).second;
-		}
-		std::cout<<std::endl;
-	}
+SQL::SQL() :
+		statement('0'), flag_count(0), flag_all(0) {
+}
+SQL::~SQL() {
+}
 
+void SQL::printAll() {
+	std::cout << this->statement << "flag_count " << this->flag_count
+			<< "flag_all " << this->flag_all << "\n";
+	for (auto a = this->expression.begin(); a != this->expression.end(); ++a) {
+		std::cout << (*a).first << " " << (*a).second;
+	}
+	std::cout << "\n";
+	for (auto a = this->where_condition.begin();
+			a != this->where_condition.end(); ++a) {
+		std::cout << (*a).first << " " << (*a).second;
+	}
+	std::cout << std::endl;
+}
 
 std::vector<std::pair<std::string, std::string>> SQL::buildPairVector(
 		std::string str) {
@@ -30,7 +34,9 @@ std::vector<std::pair<std::string, std::string>> SQL::buildPairVector(
 	std::sregex_iterator pos_u(str.cbegin(), str.cend(), e);
 	std::sregex_iterator end_u;
 	for (; pos_u != end_u; pos_u++) {
-		vps.push_back(std::pair<std::string, std::string>(pos_u->str(1), pos_u->str(2)));
+		vps.push_back(
+				std::pair<std::string, std::string>(pos_u->str(1),
+						pos_u->str(2)));
 	}
 	return vps;
 }
@@ -47,56 +53,71 @@ std::vector<std::pair<std::string, std::string>> SQL::buildStringVector(
 	return vps;
 }
 
-
 bool SQL::readSQL(std::string str, std::string name_of_table) {
-	this->flag_all=false;
-	this->flag_count=false;
+	this->flag_all = false;
+	this->flag_count = false;
 	transform(str.begin(), str.end(), str.begin(), ::toupper);
 	std::smatch m;
-
-	int pos = str.find("WHERE");
-	if (pos != -1) {
-		std::string s_where_expression = str.substr(pos, str.size() - pos);
-		this->where_condition = buildPairVector(s_where_expression);
-		if (this->where_condition.empty() == true) {
-			std::cout << "not correct statement" << std::endl;
-			return false;
-		}
-	}
 
 	if (regex_match(str,
 			std::regex(
 					"delete[[:s:]]+from[[:s:]]+" + name_of_table
 							+ "[[:s:]]*(where[[:s:]]+[[:print:]]*)?",
-							std::regex_constants::icase)) == true) {
+					std::regex_constants::icase)) == true) {
 		this->statement = 'D';
+		int pos = str.find("WHERE");
+		if (pos != -1) {
+			std::string s_where_expression = str.substr(pos, str.size() - pos);
+			this->where_condition = buildPairVector(s_where_expression);
+			if (this->where_condition.empty() == true) {
+				//std::cout << "not correct statement" << std::endl;
+				return false;
+			}
+		}
 	} else if (regex_search(str, m,
 			std::regex(
 					"insert[[:s:]]+into[[:s:]]+" + name_of_table
 							+ "[[:s:]]+values[[:s:]]+\\\({1}[[:s:]]*([[:print:]]+)\\\){1}",
-							std::regex_constants::icase)) == true) {
+					std::regex_constants::icase)) == true) {
 		this->statement = 'I';
 		this->expression = buildStringVector(m[1]);
-
+		int pos = str.find("WHERE");
+		if (pos != -1) {
+			std::string s_where_expression = str.substr(pos, str.size() - pos);
+			this->where_condition = buildPairVector(s_where_expression);
+			if (this->where_condition.empty() == true) {
+				//std::cout << "not correct statement" << std::endl;
+				return false;
+			}
+		}
 	} else if (regex_match(str,
 			std::regex(
 					"update[[:s:]]*" + name_of_table
-							+ "[[:s:]]+set[[:s:]]+(([[:w:]]+)[[:s:]]*=[[:s:]]*([[:w:]]+)*)[[:s:]]*(where[[:s:]]+[[:print:]]*)?",
-							std::regex_constants::icase)) == true) {
-		std::cout << "update u" << str <<std:: endl;
+							+ "[[:s:]]+set[[:s:]]+(([[:w:]]+)[[:s:]]*=[[:s:]]*([[:w:]]+)+)[[:s:]]*(where[[:s:]]+[[:print:]]*)?",
+					std::regex_constants::icase)) == true) {
+		//std::cout << "update u" << str << std::endl;
 		regex_search(str, m,
 				std::regex(
 						"update[[:s:]]*" + name_of_table
 								+ "[[:s:]]+set([[:print:]]*)[[:s:]]+",
-								std::regex_constants::icase));
+						std::regex_constants::icase));
 		this->statement = 'U';
 		this->expression = buildPairVector(m[1]);
+		int pos = str.find("WHERE");
+		if (pos != -1) {
+			std::string s_where_expression = str.substr(pos, str.size() - pos);
+			this->where_condition = buildPairVector(s_where_expression);
+			if (this->where_condition.empty() == true) {
+				//std::cout << "not correct statement" << std::endl;
+				return false;
+			}
+		}
 	} else if (regex_match(str, m,
 			std::regex(
 					"select[[:s:]]+([[:print:]]+)[[:s:]]+from[[:s:]]+"
 							+ name_of_table
 							+ "[[:s:]]*(where[[:s:]]+[[:print:]]*)?",
-							std::regex_constants::icase)) == true) {
+					std::regex_constants::icase)) == true) {
 		this->statement = 'S';
 		if (m[1] == "*") {
 			this->flag_all = true;
@@ -104,6 +125,15 @@ bool SQL::readSQL(std::string str, std::string name_of_table) {
 			this->flag_count = true;
 		} else {
 			this->expression = buildStringVector(m[1]);
+		}
+		int pos = str.find("WHERE");
+		if (pos != -1) {
+			std::string s_where_expression = str.substr(pos, str.size() - pos);
+			this->where_condition = buildPairVector(s_where_expression);
+			if (this->where_condition.empty() == true) {
+				//std::cout << "not correct statement" << std::endl;
+				return false;
+			}
 		}
 	} else {
 		return false;
